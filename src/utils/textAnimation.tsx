c0c163c180756c63a63c738fb5b90676a7369df8
tahar belghitri <tahartlm0@gmail.com>
2022-09-27 23:31:12 +0100

    Update textAnimation.tsx

--- src/utils/textAnimation.tsx
+++ src/utils/textAnimation.tsx
@@ -1,7 +1,7 @@
 import { Box, ChakraProps, HStack, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
-export default (props: ChakraProps & { content: string }) => {
+export default (props: ChakraProps & { content: string; delay: number }) => {
   return (
     <HStack spacing="0" flexWrap="wrap" justifyContent="center">
       {props.content.split(" ").map((e, i) => {
@@ -22,7 +22,7 @@ export default (props: ChakraProps & { content: string }) => {
                 top: "0px",
                 opacity: 1,
                 transform: "rotate(0deg)",
-                transition: { type: "just", delay: 0.015 * i },
+                transition: { type: "just", delay: 0.015 * i + props.delay },
               }}
             >
               {e + "\u00a0"}
